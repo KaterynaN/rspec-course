b5a7397eba90367488d5f78d92ff938d20d5b071
KaterynaN <kateryna.od.ua@gmail.com>
2022-02-10 18:10:57 +0200

    Add custom error text to card_spec.rb

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -25,6 +25,12 @@ RSpec.describe 'Card' do
     it 'has a suit' do
         expect(card.suit).to eq('Spades')
     end
+
+    it 'has a custom error message' do
+        card.suit = 'Nonsence'
+        comparison = 'Spades'
+        expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead"
+    end
 end
 
 # RSpec.describe 'MathCalculations' do
