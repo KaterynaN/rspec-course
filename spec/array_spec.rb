166e052e4db43b7d3afeb3c3414cbb34aef49a15
KaterynaN <kateryna.od.ua@gmail.com>
2022-02-10 23:17:24 +0200

    explicite subject

---
+++ spec/array_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Array do
+    it 'should start off empty' do
+        puts subject
+        puts subject.class
+        expect(subject.length).to eq(0)
+        subject.push(1)
+        expect(subject.length).to eq(1)
+    end
+end
